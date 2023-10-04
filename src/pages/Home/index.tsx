3a6c4af2815c17000f6001b3d51d3b38633b110b
Abhinav Awasthi <awasthiabhinav744@gmail.com>
2023-10-05 01:13:42 +0530

    updated new event dsa mern masterclass

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -27,43 +27,23 @@ const Home = () => {
         id="intro"
       />
       <iframe
-  src="https://lu.ma/embed-checkout/evt-JI6MLUama5KQDzX"
-  width="100%"
+      title="crackDSA DSA & MERN Masterclass"
+        src="https://lu.ma/embed-checkout/evt-suiXAIArdcuVKR3"
+        width="100%"
         height="600"
         frameBorder="0"
         style={{ border: "1px solid #bfcbda88", borderRadius: "4px" }}
         allowFullScreen={false}
         aria-hidden="false"
         tabIndex={0}
-></iframe>
-      
+      ></iframe>
+
       <MiddleBlock
         title={MiddleBlockContent.title}
         content={MiddleBlockContent.text}
         button={MiddleBlockContent.button}
       />
 
-      {/* <iframe
-        src="https://lu.ma/crackdsa"
-        width="100%"
-        height="600"
-        frameBorder="0"
-        allowFullScreen={false}
-        aria-hidden="false"
-        tabIndex={0}
-        style={{ border: "1px solid #bfcbda8", borderRadius: "4px" }}
-
-      ></iframe> */}
-      {/* <iframe
-        src="https://lu.ma/embed-checkout/evt-nsiCjDl42pqNlAo"
-        width="100%"
-        height="550"
-        frameBorder="0"
-        style={{ border: "1px solid #bfcbda88", borderRadius: "4px" }}
-        allowFullScreen={false}
-        aria-hidden="false"
-        tabIndex={0}
-      ></iframe> */}
       <ContentBlock
         type="left"
         title={AboutContent.title}
